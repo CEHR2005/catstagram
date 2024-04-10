1a271b40756e794a116cc18a263b4f68b79c83a0
Arsentii Bieliaiev <SenchaPencha1@gmail.com>
2024-04-11 02:05:37 +0300

    update css

--- src/components/ui/Gallery.tsx
+++ src/components/ui/Gallery.tsx
@@ -4,19 +4,21 @@ function ImageGallery() {
     const [images, setImages] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:3000/posts/images')
+        fetch('http://localhost:3001/posts/images')
             .then(response => response.json())
             .then(data => setImages(data))
             .catch(error => console.error(error));
     }, []);
 
     return (
-        <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
             {images.map((imageUrl, index) => (
-                <img key={index} src={imageUrl} alt="" loading="lazy" />
+                <div key={index} className="overflow-hidden rounded-lg shadow-lg">
+                    <img src={imageUrl} alt="" loading="lazy" className="w-full h-auto object-cover object-center" />
+                </div>
             ))}
         </div>
     );
 }
 
-export {ImageGallery};
+export { ImageGallery };
