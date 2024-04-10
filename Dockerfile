79c39a0e3047df40cdf2e39bb1bb0654bb71ad6d
Arsentii Bieliaiev <SenchaPencha1@gmail.com>
2024-04-11 02:04:13 +0300

    routs for docker

--- Dockerfile
+++ Dockerfile
@@ -20,9 +20,6 @@ RUN ls -la /app
 # Использование образа Nginx для сервирования контента
 FROM nginx:stable-alpine
 COPY --from=build /app/dist /usr/share/nginx/html
-
-# Открытие 80 порта для HTTP трафика
+COPY default.conf /etc/nginx/conf.d/default.conf
 EXPOSE 80
-
-# Запуск Nginx
 CMD ["nginx", "-g", "daemon off;"]
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import '../app/globals.css'
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+import {z} from "zod"
 
-import { Button } from "@/components/ui/button"
+import {Button} from "@/components/ui/button"
 import {
     Form,
     FormControl,
@@ -13,19 +13,21 @@ import {
     FormLabel,
     FormMessage,
 } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { toast } from "@/components/ui/use-toast"
+import {Input} from "@/components/ui/input"
+import {toast} from "@/components/ui/use-toast"
 import {Label} from "@/components/ui/label.tsx";
 import {ImageGallery} from "@/components/ui/Gallery.tsx";
 import {
     NavigationMenu,
-    NavigationMenuContent,
     NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
-    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
-import { Routes, Route } from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
+import {Link} from "@radix-ui/react-navigation-menu";
+
+
 const FormSchema = z.object({
     authorName: z.string().min(2, {
         message: "Username must be at least 2 characters.",
@@ -94,89 +96,101 @@ function App() {
         }
     }
 
-  return (
-      <Routes>
-          <Route path="/" element={
-              <>
-                  <header>
-                      <NavigationMenu>
-                          <NavigationMenuList>
-                              <NavigationMenuItem>
-                                  <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                                  <NavigationMenuContent>
-                                      <NavigationMenuLink href="/about">Link</NavigationMenuLink>
-                                  </NavigationMenuContent>
-                              </NavigationMenuItem>
-                          </NavigationMenuList>
-                      </NavigationMenu>
-                  </header>
 
-
-                  <Form {...form}>
-                      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-                          <FormField
-                              control={form.control}
-                              name="authorName"
-                              render={({field}) => (
-                                  <FormItem>
-                                      <FormLabel>authorName</FormLabel>
-                                      <FormControl>
-                                          <Input placeholder="" {...field} />
-                                      </FormControl>
-                                      <FormDescription>
-                                      </FormDescription>
-                                      <FormMessage/>
-                                  </FormItem>
-                              )}
-                          />
-                          <FormField
-                              control={form.control}
-                              name="authorEmail"
-                              render={({field}) => (
-                                  <FormItem>
-                                      <FormLabel>authorEmail</FormLabel>
-                                      <FormControl>
-                                          <Input placeholder="" {...field} />
-                                      </FormControl>
-                                      <FormDescription>
-                                      </FormDescription>
-                                      <FormMessage/>
-                                  </FormItem>
-                              )}
-                          />
-                          <FormField
-                              control={form.control}
-                              name="comment"
-                              render={({field}) => (
-                                  <FormItem>
-                                      <FormLabel>comment</FormLabel>
-                                      <FormControl>
-                                          <Input placeholder="" {...field} />
-                                      </FormControl>
-                                      <FormDescription>
-                                      </FormDescription>
-                                      <FormMessage/>
-                                  </FormItem>
-                              )}
-                          />
-                          <div className="grid w-full max-w-sm items-center gap-1.5">
-                              <Label htmlFor="picture">Picture</Label>
-                              <Input id="picture" type="file"/>
-                          </div>
-                          <Button type="submit">Submit</Button>
-                      </form>
-                  </Form>
-                  <ImageGallery/></>
-          }>
-
-          </Route>
-          <Route path="/about" element={
-              <h1>About</h1>
-          }>
-
-          </Route>
-      </Routes>
-  )
+    return (
+        <>
+            <header className="flex items-center justify-between p-6 max-w">
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <Link href="/">
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                    Posts
+                                </NavigationMenuLink>
+                            </Link>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <Link href="/post">
+                                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                    Post
+                                </NavigationMenuLink>
+                            </Link>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </header>
+            <body className="justify-center px-5 content-center ">
+            <Routes>
+                <Route path="/" element={
+                    <>
+                        <ImageGallery/>
+                    </>
+                }>
+                </Route>
+                <Route path="/post" element={
+                    <>
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+                                <FormField
+                                    control={form.control}
+                                    name="authorName"
+                                    render={({field}) => (
+                                        <FormItem>
+                                            <FormLabel>authorName</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="" {...field} />
+                                            </FormControl>
+                                            <FormDescription>
+                                            </FormDescription>
+                                            <FormMessage/>
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="authorEmail"
+                                    render={({field}) => (
+                                        <FormItem>
+                                            <FormLabel>authorEmail</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="" {...field} />
+                                            </FormControl>
+                                            <FormDescription>
+                                            </FormDescription>
+                                            <FormMessage/>
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="comment"
+                                    render={({field}) => (
+                                        <FormItem>
+                                            <FormLabel>comment</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="" {...field} />
+                                            </FormControl>
+                                            <FormDescription>
+                                            </FormDescription>
+                                            <FormMessage/>
+                                        </FormItem>
+                                    )}
+                                />
+                                <div className="grid w-full max-w-sm items-center gap-1.5">
+                                    <Label htmlFor="picture">Picture</Label>
+                                    <Input id="picture" type="file"/>
+                                </div>
+                                <Button type="submit">Submit</Button>
+                            </form>
+                        </Form></>
+                }>
+                </Route>
+            </Routes>
+            </body>
+        </>
+    )
 }
 
 export default App
